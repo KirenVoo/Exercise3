f32ab842e690ce1a626242107d5459f111c342e0
KirenVoo <vooky-sm17@student.tarc.edu.my>
2020-01-12 00:30:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/exercise3/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.exercise3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Spinner
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonCalculate.setOnClickListener{
+
+            var premium = 0
+            val spinnerAge = findViewById<Spinner>(R.id.spinnerAge)
+
+            if(spinnerAge.selectedItem.equals("Less than 17")){
+                premium += 60
+            }
+            else if(spinnerAge.selectedItem.equals("17 to 25")){
+                premium += 70
+                if(radioButtonMale.isChecked){
+                    premium += 50
+                }
+                if(checkBoxSmoker.isChecked){
+                    premium += 100
+                }
+            }
+            else if(spinnerAge.selectedItem.equals("26 to 30")){
+                premium += 90
+                if(radioButtonMale.isChecked){
+                    premium += 100
+                }
+                if(checkBoxSmoker.isChecked){
+                    premium += 150
+                }
+            }
+            else if(spinnerAge.selectedItem.equals("31 to 40")){
+                premium += 120
+                if(radioButtonMale.isChecked){
+                    premium += 150
+                }
+                if(checkBoxSmoker.isChecked){
+                    premium += 200
+                }
+            }
+            else if(spinnerAge.selectedItem.equals("41 to 55")){
+                premium += 150
+                if(radioButtonMale.isChecked){
+                    premium += 200
+                }
+                if(checkBoxSmoker.isChecked){
+                    premium += 250
+                }
+            }
+            else if(spinnerAge.selectedItem.equals("More than 55")){
+                premium += 150
+                if(radioButtonMale.isChecked){
+                    premium += 200
+                }
+                if(checkBoxSmoker.isChecked){
+                    premium += 300
+                }
+            }
+            textViewPremium.setText("RM " + premium.toString())
+        }
+        buttonReset.setOnClickListener {
+            textViewPremium.setText("Insurance Premium :")
+        }
+    }
+}
